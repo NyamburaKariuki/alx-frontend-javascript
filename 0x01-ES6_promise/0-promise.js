eadaca2808aad27af630234dbab0f30a17496246
NyamburaKariuki <prettimercii@gmail.cm>
2024-08-01 09:04:59 +0300

    packages

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,4 @@
-function APIHandler() {}
-
-APIHandler.prototype.getResponseFromAPI = function() {
-    return new Promise((resolve, reject) => {
-        // Simulate an API call
-        setTimeout(() => {
-            const success = true; // Change this to simulate success or failure
-            if (success) {
-                resolve("API call successful");
-            } else {
-                reject("API call failed");
-            }
-        }, 2000); // Simulate a 2-second delay for the API call
-    });
-};
+export default function getResponseFromAPI() {
+  return new Promise(() => {
+  });
+}
