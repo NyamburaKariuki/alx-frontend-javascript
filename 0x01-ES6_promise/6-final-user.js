df7128ace1bf2fac43fe64ca44c60008ad18ce5d
NyamburaKariuki <prettimercii@gmail.com>
2024-08-01 08:32:10 +0000

    an array of promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  
   return Promise
     // Call the signUpUser() and uploadPhoto() functions
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
