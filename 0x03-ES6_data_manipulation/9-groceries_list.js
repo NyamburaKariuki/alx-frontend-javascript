303f7ea1c8250b9f993cd6687b6652f4b1a56a75
NyamburaKariuki <prettimercii@gmail.com>
2024-08-02 13:06:16 +0000

    map data structuire

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,14 +1,14 @@
 export default function groceriesList() {
   // Create a new Map
   const map = new Map();
-  
+
   // Add items to the Map
   map.set('Apples', 10);
   map.set('Tomatoes', 10);
   map.set('Pasta', 1);
   map.set('Rice', 1);
   map.set('Banana', 5);
-  
+
   // Return the Map
   return map;
 }
