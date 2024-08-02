3530a75d413c9a612f65efb894c2c396c3981860
NyamburaKariuki <prettimercii@gmail.com>
2024-08-02 12:58:10 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,22 @@
 export default function cleanSet(set, startString) {
-    // Filter values in the set that start with startString and remove startString
-    const result = Array.from(set)
-        .filter(value => typeof value === 'string' && value.startsWith(startString))
-        .map(value => value.slice(startString.length))
-        .join('-');
-    
-    return result;
+  // Array to store the remaining values
+  const parts = [];
+  // Return an empty string if any of the parameters are invalid
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    // Check if the value is a string and starts with the given string
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      // Extract the substring after the startString
+      const valueSubStr = value.substring(startString.length);
+
+      // Add the substring to the parts array if it is not empty and not
+      // equal to the original value
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
