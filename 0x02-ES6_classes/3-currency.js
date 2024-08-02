085666819542d79a472f83070dc9ae0bb3a3df97
NyamburaKariuki <prettimercii@gmail.com>
2024-08-02 07:20:45 +0000

    currency class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,38 +1,39 @@
+/* eslint-disable no-underscore-dangle */
 export default class Currency {
-  // Constructor initializes code and name
+  /**
+   * @param {string} code - currency code
+   * @param {string} name - currency name
+   *
+   * Implement getters and setters for each
+   */
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    this.code = code;
+    this.name = name;
   }
 
-  // Getter for code
   get code() {
     return this._code;
   }
 
-  // Setter for code
   set code(value) {
     if (typeof value !== 'string') {
-    throw new TypeError('code must be a string');
+      throw new Error('code must be a string');
     }
     this._code = value;
   }
 
-  // Getter for name
   get name() {
     return this._name;
   }
 
-  // Setter for name
   set name(value) {
-    if (typeof !== 'string') {
-    throw new TypeError('name must be  string');
+    if (typeof value !== 'string') {
+      throw new Error('name must be a string');
     }
     this._name = value;
   }
 
-  // Method to display currency in the format name (code)
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
   }
 }
