8535d39d8d7da9b003bad681eba2b7cb0f22d088
NyamburaKariuki <prettimercii@gmail.cm>
2024-08-13 14:21:50 +0300

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+    constructor(sqft) {
+        if (new.target === Building) {
+            throw new Error("Building is an abstract class and cannot be instantiated directly.");
+        }
+        this._sqft = sqft;
+    }
+
+    // Getter for sqft attribute
+    get sqft() {
+        return this._sqft;
+    }
+
+    // Abstract method (must be implemented by subclasses)
+    evacuationWarningMessage() {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
+}
